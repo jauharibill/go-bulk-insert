f1d303f6e1ecc4556c452f7825f39d50920377a6
Bill Tanthowi Jauhari <bill.tanthowi@majoo.id>
2020-08-25 08:49:14 +0700

    add partial insert

--- utils/agregator.go
+++ utils/agregator.go
@@ -1,15 +1,11 @@
 package utils
 
-func (s *BulkInsertStruct) Test() (i *BulkInsertStruct) {
-	return
-}
-
 func (t *BulkInsertStruct) SetColumn(columns string) (s *BulkInsertStruct) {
 	t.Columns = &columns
 	return t
 }
 
-func (t *BulkInsertStruct) SetValues(values string) (s *BulkInsertStruct) {
+func (t *BulkInsertStruct) SetValues(values []string) (s *BulkInsertStruct) {
 	t.Values = &values
 	return t
 }
@@ -23,7 +19,7 @@ func (t *BulkInsertStruct) GetColumn() string {
 	return *t.Columns
 }
 
-func (t *BulkInsertStruct) GetValues() string {
+func (t *BulkInsertStruct) GetValues() []string {
 	return *t.Values
 }
 
--- utils/collector.go
+++ utils/collector.go
@@ -2,13 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 )
 
 type BulkInsertStruct struct {
 	Columns *string
 	Query   *string
-	Values  *string
+	Values  *[]string
 }
 
 func (t *BulkInsertStruct) ExtractModelField(model interface{}, exceptionalField []string) (s *BulkInsertStruct) {
@@ -27,48 +28,81 @@ func (t *BulkInsertStruct) ExtractModelField(model interface{}, exceptionalField
 			for _, item := range exceptionalField {
 				if current_field != item {
 					if (i + 1) < reflectValue.NumField() {
-						columns += fmt.Sprintf("'%v', ", current_field)
+						columns += fmt.Sprintf("`%v`, ", current_field)
 					} else {
-						columns += fmt.Sprintf("'%v'", current_field)
+						columns += fmt.Sprintf("`%v`", current_field)
 					}
 				}
 			}
 		} else {
 			if (i + 1) < reflectValue.NumField() {
-				columns += fmt.Sprintf("'%v', ", current_field)
+				columns += fmt.Sprintf("`%v`, ", current_field)
 			} else {
-				columns += fmt.Sprintf("'%v'", current_field)
+				columns += fmt.Sprintf("`%v`", current_field)
 			}
 		}
-
 	}
 	t.SetColumn(columns)
 	return t
 }
 
-func (t *BulkInsertStruct) ArrangeValue(models [][]interface{}) (s *BulkInsertStruct) {
-	var values string
-	for indexCols, cols := range models {
-		var valueRows string
-		for indexRows, rows := range cols {
-			if (indexRows + 1) < len(cols) {
-				valueRows += fmt.Sprintf("'%v', ", rows)
-			} else {
-				valueRows += fmt.Sprintf("'%v'", rows)
+func (t *BulkInsertStruct) ArrangeValue(models [][]interface{}, limitPerInsert int) (s *BulkInsertStruct) {
+	var totalRows int = len(models)
+	var totalPartialInsert float64
+	var totalPartialValues []int
+	var dataPartialValues []string
+
+	if totalRows > limitPerInsert {
+		lastValue := totalRows % limitPerInsert
+		totalPartialInsert = float64(totalRows) / float64(limitPerInsert)
+		if lastValue != 0 {
+			totalPartialInsert = math.Ceil(float64(totalPartialInsert))
+		} else {
+			totalPartialInsert = math.Floor(float64(totalPartialInsert))
+		}
+		totalPartialValues = make([]int, int(totalPartialInsert))
+		for i := 0; i < int(totalPartialInsert); i++ {
+			totalPartialValues[i] = limitPerInsert
+			if (i+1) == int(totalPartialInsert) && lastValue != 0 {
+				totalPartialValues[i] = lastValue
 			}
 		}
-
-		if (indexCols + 1) < len(models) {
-			values += fmt.Sprintf("(%v),", valueRows)
-		} else {
-			values += fmt.Sprintf("(%v);", valueRows)
-		}
 	}
-	t.SetValues(values)
+
+	var totalItem int
+	dataPartialValues = make([]string, len(totalPartialValues))
+	for index, item := range totalPartialValues {
+		var values string
+		for indexCols := 0; indexCols < item; indexCols++ {
+			var valueRows string
+			current_rows := models[totalItem+indexCols]
+			for indexRows, rows := range current_rows {
+				if (indexRows + 1) < len(current_rows) {
+					valueRows += fmt.Sprintf("'%v', ", rows)
+				} else {
+					valueRows += fmt.Sprintf("'%v'", rows)
+				}
+			}
+
+			if (indexCols + 1) < item {
+				values += fmt.Sprintf("(%v),", valueRows)
+			} else {
+				values += fmt.Sprintf("(%v)", valueRows)
+			}
+		}
+		dataPartialValues[index] = values
+		totalItem += item
+	}
+
+	t.SetValues(dataPartialValues)
 	return t
 }
 
-func (t *BulkInsertStruct) PutQueryTogether(tableName string) (s *BulkInsertStruct) {
-	t.SetQuery(fmt.Sprintf("INSERT INTO %v (%v) VALUES %v", tableName, t.GetColumn(), t.GetValues()))
+func (t *BulkInsertStruct) InsertQuery(tableName string) (s *BulkInsertStruct) {
+	var query string
+	for _, item := range t.GetValues() {
+		query += fmt.Sprintf("INSERT INTO `%v` (%v) VALUES %v;\n", tableName, t.GetColumn(), item)
+	}
+	t.SetQuery(query)
 	return t
 }
